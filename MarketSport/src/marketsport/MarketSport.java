f0fea18db4bea05c4cb10947247a72f47f904986
sebasRR29 <oritooritoringo@gmail.com>
2025-10-08 01:23:17 +0200

    Add files via upload

---
+++ MarketSport/src/marketsport/MarketSport.java
@@ -0,0 +1,13 @@
+
+package marketsport;
+
+public class MarketSport {
+    public static void main(String[] args) {
+        
+        Inventario i = new Inventario();
+        Persona p = new Persona("sebas", 100000);
+        i.vender(p);
+        
+    }
+    
+}
